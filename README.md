36db675e6e60e0ad49aab7b8ff1d9a7a51acbb0b
Tristan <tristan.perrot@telecom-paris.fr>
2023-05-17 16:49:28 +0200

    finished step2

--- README.md
+++ README.md
@@ -25,3 +25,64 @@ J'ai donc implémenter un logiciel en java qui compte le nombre d’occurrences 
     3. sont
     4. directeur
     5. conditions
+* Pour l'extrait de page internet, j'ai obtenu ces données :
+  * Durée de lecture et de séparation : 4.039s
+  * Durée de comptage : 5.271s
+  * Durée de tri et d'affichage des 50 premiers mots : 6.809s
+
+## Étape 2
+
+Pour l'étape 2, j'ai utilisé l'ordinateur **tp-1a201-12** (tp-1a201-12.enst.fr). 
+Pour trouver son nom à l'aide d'une commande il faut écrire :
+
+```bash
+hostname -f
+```
+
+> le -f permet d'avoir le nom complet (avec le nom de domaine).
+
+Sur les ordinateurs de l'école je crois que cela est écrit un peu partout.
+
+Pour trouver les addresses IP des ordinateurs de l'école, il faut utiliser la commande :
+
+```bash
+ifconfig
+```
+
+Pour obtenir les adresses IP en ligne de commande à partir du nom d'un ordinateur il faut utiliser la commande :
+
+```bash
+host <nom de l'ordinateur>
+```
+
+Pour obtenir les noms associés en ligne de commande à partir d'une adresse IP il faut utiliser la commande :
+
+```bash
+host <adresse IP>
+```
+
+Les appels à la fonction ping avec comme argument le nom court, le nom long ou bien l'adresse IP fonctionnent très bien.
+
+Pour lancer un calcul en ligne de commande, il est possible de faire comme cela:
+
+```bash
+echo $((2 + 3))
+```
+
+> Par ailleurs, cette commande permet d'avoir le résultat d'un calcul avec un seul appuie sur la touche entrée.
+
+Pour lancer ce calcul à distance, nous pouvons procéder à l'aide de *ssh* comme cela :
+
+```bash
+ssh <nom de l'ordinateur> echo $((2 + 3))
+```
+
+> Par ailleurs, cette commande permet d'avoir le résultat d'un calcul immédiatement après saisie du mot de passe.
+
+Pour ne plus avoir à faire à ce mot de passe il suffit d'utiliser d'enregistrer sa clé publique sur l'ordinateur distant. Pour cela il faut utiliser la commande :
+
+```bash
+ssh-copy-id <nom de l'ordinateur>
+```
+
+## Étape 3
--- Step1/CountWords.java
+++ Step1/CountWords.java
@@ -15,37 +15,43 @@ public class CountWords {
     private String[] words;
 
     public CountWords(String path, int limit) {
+        long startTime = System.currentTimeMillis();
         text = readFile(path);
         words = text.split(" ");
+        long endTime = System.currentTimeMillis();
+        System.out.println("\nTook: " + (endTime - startTime) + " ms to read & split\n");
 
         System.out.println(
-                "\nStarting to count words...");
-        long startTime = System.currentTimeMillis();
+                "Starting to count words...");
+        long startTime2 = System.currentTimeMillis();
         HashMap<String, Integer> wordsMap = countWordsFreq();
-        long endTime = System.currentTimeMillis();
-        System.out.println("\nTook: " + (endTime - startTime) + " ms to count\n");
-        printFreq(wordsMap, limit);
         long endTime2 = System.currentTimeMillis();
-        System.out.println("\nTook: " + (endTime2 - endTime) + " ms to print\n");
+        System.out.println("\nTook: " + (endTime2 - startTime2) + " ms to count\n");
+        System.out.println("\nTook: " + (endTime2 - startTime2 + endTime - startTime) + " ms to read & count\n");
+        long startTime3 = System.currentTimeMillis();
+        printFreq(wordsMap, limit);
+        long endTime3 = System.currentTimeMillis();
+        System.out.println("\nTook: " + (endTime3 - startTime3) + " ms to print\n");
     }
 
     private String readFile(String path) {
         String content = new String();
         try {
             File file = new File(path);
-            System.out.println(
-                    "\n################################### " + file.getName()
-                            + " ###################################\n");
-            BufferedReader br;
-            br = new BufferedReader(new FileReader(file));
+            System.out.println("\n################################### " + file.getName()
+                    + " ###################################\n");
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
-            // doing a loading bar
+
+            StringBuilder contentBuilder = new StringBuilder(); // Use StringBuilder instead of concatenating strings
             int i = 0;
             int size = (int) file.length();
             int step = size / 100;
             int progress = 0;
+
             while ((line = br.readLine()) != null) {
-                content += " " + line;
+                contentBuilder.append(" ").append(line); // Append the line to the StringBuilder
                 i += line.length();
                 if (i > progress) {
                     System.out.print("\r");
@@ -53,6 +59,8 @@ public class CountWords {
                     progress += step;
                 }
             }
+
+            content = contentBuilder.toString(); // Convert StringBuilder to a string
             System.out.print("\nLoaded\n");
             br.close();
         } catch (Exception e) {
@@ -97,14 +105,13 @@ public class CountWords {
     }
 
     public static void main(String[] args) {
-        //new CountWords("Step1/data/input.txt", -1);
-        //new CountWords("Step1/data/forestier_mayotte.txt", -1);
-        //new CountWords("Step1/data/deontologie_police_nationale.txt", 50);
-        //new CountWords("Step1/data/domaine_public_fluvial.txt", 50);
-        //new CountWords("Step1/data/sante_publique.txt", 50);
-        // new
-        // CountWords("/tmp/slr207/CC-MAIN-20230320083513-20230320113513-00000.warc.wet",
-        // 50);
+        // new CountWords("Step1/data/input.txt", -1);
+        // new CountWords("Step1/data/forestier_mayotte.txt", -1);
+        // new CountWords("Step1/data/deontologie_police_nationale.txt", 50);
+        // new CountWords("Step1/data/domaine_public_fluvial.txt", 50);
+        // new CountWords("Step1/data/sante_publique.txt", 50);
+        new CountWords("/tmp/slr207/CC-MAIN-20230320083513-20230320113513-00000.warc.wet",
+                50);
     }
 
 }
