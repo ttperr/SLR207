1a08dd5233a8fba96a4005504d13fb0db8759a4b
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-05-25 13:38:31 +0200

    finished master step10 (untested)

--- Step10/MASTER.java
+++ Step10/MASTER.java
@@ -1,13 +1,11 @@
 package Step10;
 
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.Map;
-import org.json.JSONObject;
 
 public class MASTER {
     private static final String USERNAME = "tperrot-21";
@@ -61,40 +59,39 @@ public class MASTER {
             System.err.println("Le répertoire " + SPLIT_DIRECTORY + " n'existe pas.");
             return;
         }
-        
+
         // Check if there are files in the splits directory
         File[] splitFiles = splitsDirectory.listFiles();
-        if (splitFiles.length == 0) {
+
+        if (splitFiles == null || splitFiles.length == 0) {
             System.err.println("Le répertoire " + SPLIT_DIRECTORY + " est vide.");
             return;
         }
 
         // Check if there is the same number of files than the number of machines
-        if (splitFiles.length != computers.length()) {
+        if (splitFiles.length != computers.length) {
             System.err.println("Le nombre de fichiers dans le répertoire " + SPLIT_DIRECTORY + " n'est pas égal au nombre de machines.");
             return;
         }
-        
-        Map<String, String> splits = Map.of("S0.txt", "Deer", "S1.txt", "Car", "S2.txt", "Deer Car Beer");
 
         computers.keySet().forEach(machineNumber -> {
             String ipAddress = computers.getString(machineNumber);
             String machineDirectory = String.format("%s@%s:%s", USERNAME, ipAddress, SPLIT_DIRECTORY);
 
-            splits.forEach((splitFile, splitContent) -> {
-                if (splitContent.contains(machineNumber)) {
-                    try {
-                        // Copier le fichier split vers la machine distante
-                        Path sourcePath = Path.of(splitFile);
-                        Path targetPath = Path.of(machineDirectory, splitFile);
+            try {
+                // Copier le fichier sur la machine distante
+                ProcessBuilder pb = new ProcessBuilder("scp", splitFiles[Integer.parseInt(machineNumber) - 1].getAbsolutePath(), machineDirectory);
+                Process process = pb.start();
+                int exitCode = process.waitFor();
 
-                        Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
-                        System.out.println("Copie du fichier " + splitFile + " vers la machine " + machineNumber + ": " + ipAddress);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                if (exitCode == 0) {
+                    System.out.println("Fichier copié sur la machine " + machineNumber + ": " + ipAddress);
+                } else {
+                    System.err.println("Erreur lors de la copie du fichier sur la machine " + machineNumber + ": " + ipAddress);
                 }
-            });
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         });
     }
 }
