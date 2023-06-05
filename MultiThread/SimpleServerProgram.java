67748b5a49091ce8c10476591bbe786cb77917d3
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-06-05 11:29:57 +0200

    cleanup

--- MultiThread/SimpleServerProgram.java
+++ MultiThread/SimpleServerProgram.java
@@ -10,7 +10,7 @@ import java.net.Socket;
 
 public class SimpleServerProgram {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         ServerSocket listener = null;
         String line;
