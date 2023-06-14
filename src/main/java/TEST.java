a3a4480af6b3d07310594defde243d9d6b9ed02e
Tristan <tt.perrot@gmail.com>
2023-06-14 19:38:53 +0200

    fixing

--- src/main/java/TEST.java
+++ src/main/java/TEST.java
@@ -1,12 +1,10 @@
-import java.io.IOException;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class TEST {
     public static void main(String[] args) throws UnknownHostException {
         InetAddress localhost = InetAddress.getLocalHost();
-            String hostName = localhost.getHostName();
+            String hostName = localhost.getCanonicalHostName();
             System.out.println("Host name: " + hostName);
     }
 }
